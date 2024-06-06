b34b9ce269ab10d2aafdde94185d7ea7d183b09e
javd boroji <javadboroji22222@gmail.com>
2024-06-07 02:16:58 +0330

    add Login Form

--- vite-number-conversion/src/Pages/Components/Forms/Forms.basic.tsx
+++ vite-number-conversion/src/Pages/Components/Forms/Forms.basic.tsx
@@ -10,19 +10,45 @@ type RegisterInputs={
     email:string,
     password:string
 }
+type LoginInputs={
+    email:string,
+    password:string
+}
+type ContactInputs={
+    userName:string,
+    email:string,
+    title:string,
+    message:string
+}
 type registerFormType={
     registerHandler:(data:RegisterInputs)=>void  
 }
+type loginFormType={
+    loginHandler:(data:LoginInputs)=>void
+}
+type contactFormType={
+    contactHandler:(data:ContactInputs)=>void
+}
+
 function FormsBas() {
     const register=(data:RegisterInputs)=>{
         console.log(data);
         
     }
+    const login=(data:LoginInputs)=>{
+        console.log(data);
+        
+    }
   return (
-    <Row className='w-full p-4'>
-      <Col xs={24} md={12}>
+    <Row className='w-full m-4 flex justify-between' gutter={0}>
+      <Col xs={24} md={11} >
         <span className='text-lg text-orang-100 p-2 text-center block'> فرم ثبت نام  </span>
          <RegisterForm registerHandler={register} />
+
+      </Col>
+      <Col xs={24} md={11}>
+      <span className='text-lg text-orang-100 p-2 text-center block'> فرم ورود  </span>
+        <LoginForm loginHandler={login}/>
       </Col>
     </Row>
   )
@@ -66,4 +92,43 @@ const RegisterForm=(props:registerFormType)=>{
             
         </form>
     )
+}
+
+const LoginForm=(props:loginFormType)=>{
+
+    
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+      } = useForm<LoginInputs>()
+
+  
+    return(
+        <form className='w-full p-12 rounded-lg shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]' onSubmit={handleSubmit((data:LoginInputs)=>props.loginHandler(data)) }>
+            <Row gutter={[12,12]}>
+             
+                <Col xs={24} >
+                <input  className='form-inp-cus' placeholder='ایمیل' type='email' {...register('email',{required:true})}/>
+                {errors.email && <span className='text-red-600 text-sm p-2'> ایمیل   اجباری است</span>}
+                </Col>
+                <Col xs={24} >
+                <input  className='form-inp-cus' placeholder='رمز عبور' type='password' {...register('password',{required:true})}/>
+                {errors.password && <span className='text-red-600 text-sm p-2'>  پسورد  اجباری است</span>}
+                </Col>
+                <Col xs={24} md={24} className='flex items-center justify-center'>
+                    
+                    <BtnBasComponent basButtonsType='primary' btnTypeSubmit={true}  dyStyle={'text-white px-20 '} btnText='ورود'  size='md' />
+                </Col>
+            </Row>
+        
+         
+            
+        </form>
+    )
+}
+
+const ContactForm=(props:)=>{
+
 }

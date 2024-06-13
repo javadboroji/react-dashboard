8e597e709d5f94950578a342d310ebd51dada1ce
javd boroji <javadboroji22222@gmail.com>
2024-06-13 23:12:16 +0330

    add shopp card

--- vite-number-conversion/src/Pages/Components/Cards/Cards.basic.tsx
+++ vite-number-conversion/src/Pages/Components/Cards/Cards.basic.tsx
@@ -2,38 +2,44 @@ import { Col, Row } from 'antd'
 import React from 'react'
 import card from "../../../assets/card.png";
 import profile2 from '../../../../public/images/us-2.jpg';
+import product from '../../../assets/product.jpg'
 import "./Card.Style.css"
 import { Link } from 'react-router-dom';
+import BtnBasComponent from '../Buttons/BtnBasComponent';
 function CardsBasic() {
     return (
         <div className='container p-6'>
-            <Row gutter={[16,16]}>
+            <Row gutter={[16, 16]}>
 
-            <CardInitial
-                cardImage={card} title='اخرین پست'
-                description='لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ  لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ '
-                textColor='text-black'
-                date='خرداد 1403 / 16:30'
-                userName='جواد بروجی'
-                profile={profile2}
-                dateColor='text-gray-400'
-                authorTextColor='text-blue-50'
-                boxShadow={true}
-                category={<span className='text-white w-fit bg-blue-50 rounded-3xl text-xs py-1 px-2'> مقالات </span>}
-            />
-            <CardWidthTags
-                cardImage={card} title='اخرین پست'
-                description='لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ  لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ '
-                textColor='text-black'
-                date='خرداد 1403 / 16:30'
-                userName='جواد بروجی'
-                profile={profile2}
-                dateColor='text-gray-400'
-                authorTextColor='text-blue-50'
-                boxShadow={true}
-                category={<span className='text-white w-fit bg-red-700 absolute top-4 left-0  rounded-tr-lg rounded-br-lg text-sm py-1 px-4'> مقالات </span>}
-                tags={['مقالات' , 'پست' ,'وب سایت', 'آموزش']}
-            />
+                <CardInitial
+                    cardImage={card} title='اخرین پست'
+                    description='لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ  لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ '
+                    textColor='text-black'
+                    date='خرداد 1403 / 16:30'
+                    userName='جواد بروجی'
+                    profile={profile2}
+                    dateColor='text-gray-400'
+                    authorTextColor='text-blue-50'
+                    boxShadow={true}
+                    category={<span className='text-white w-fit bg-blue-50 rounded-3xl text-xs py-1 px-2'> مقالات </span>}
+                />
+                <CardWidthTags
+                    cardImage={card} title='اخرین پست'
+                    description='لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ  لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ '
+                    textColor='text-black'
+                    date='خرداد 1403 / 16:30'
+                    userName='جواد بروجی'
+                    profile={profile2}
+                    dateColor='text-gray-400'
+                    authorTextColor='text-blue-50'
+                    boxShadow={true}
+                    category={<span className='text-white w-fit bg-red-700 absolute top-4 left-0  rounded-tr-lg rounded-br-lg text-sm py-1 px-4'> مقالات </span>}
+                    tags={['مقالات', 'پست', 'وب سایت', 'آموزش']}
+                />
+              
+            </Row>
+            <Row>
+            <ShoppCard orderImage={product} orderName='کلاه نقاب دار' orderPrice='150.000' priceColor='text-orang-100' category='تخفیف'/>
             </Row>
         </div>
     )
@@ -56,7 +62,15 @@ type CardInitialType = AuthorCard & {
     dateColor?: string,
     boxShadow?: true,
     category?: any,
-    tags?:string[]
+    tags?: string[]
+}
+
+type ShoppCardProps = {
+    orderImage: string,
+    orderName: string,
+    orderPrice: string,
+    priceColor?:string,
+    category?:string
 }
 const CardInitial: React.FC<CardInitialType> = ({ cardImage, title, description, textColor, date, userName, profile, dateColor, authorTextColor, boxShadow, category }) => {
 
@@ -67,6 +81,7 @@ const CardInitial: React.FC<CardInitialType> = ({ cardImage, title, description,
                     <div className={`${boxShadow && 'shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]'}  w-full rounded-lg borderCard`}>
                         <div className=' w-full relative pt-[50%] rounded-tl-lg rounded-tr-lg overflow-hidden'>
                             <img className='w-full h-full object-cover absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]' src={cardImage} alt={cardImage} />
+                            
                         </div>
                         <div className='flex flex-col px-4 py-2'>
                             {category && category}
@@ -87,7 +102,7 @@ const CardInitial: React.FC<CardInitialType> = ({ cardImage, title, description,
     )
 }
 
-const CardWidthTags: React.FC<CardInitialType> = ({ cardImage, title, description, textColor, date, userName, profile, dateColor, authorTextColor, boxShadow, category,tags }) => {
+const CardWidthTags: React.FC<CardInitialType> = ({ cardImage, title, description, textColor, date, userName, profile, dateColor, authorTextColor, boxShadow, category, tags }) => {
     return (
         <>
             <Col xs={24} md={12} lg={8}>
@@ -98,7 +113,7 @@ const CardWidthTags: React.FC<CardInitialType> = ({ cardImage, title, descriptio
                             {category && category}
                         </div>
                         <div className='flex flex-col px-4 py-2'>
-                            <div className='flex'>{tags&&tags.map((tag ,i)=><span key={i+'tag'} className='text-blue-50'> #{tag}</span>)}</div>
+                            <div className='flex'>{tags && tags.map((tag, i) => <span key={i + 'tag'} className='text-blue-50'> #{tag}</span>)}</div>
                             <p className={`${textColor ? textColor : 'text-zinc-950'} font-bold text-2xl line-clamp-1 leading-8 py-2`}> {title}</p>
                             <span className={`${textColor ? textColor : 'text-zinc-950'} text-sm line-clamp-2`}>{description}</span>
                         </div>
@@ -113,5 +128,26 @@ const CardWidthTags: React.FC<CardInitialType> = ({ cardImage, title, descriptio
                 </Link>
             </Col>
         </>
+    )
+}
+
+const ShoppCard: React.FC<ShoppCardProps> = ({ orderImage, orderName, orderPrice,priceColor,category }) => {
+    return (
+        <Col xs={24} md={12} lg={6}>
+            <div className='w-full'>
+                <div className='group w-full relative pt-[75%] overflow-hidden rounded-lg'>
+                    <img className='w-full h-full  object-cover absolute group-hover:opacity-50 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]' src={orderImage} alt={orderImage} />
+                    {category&&<span className='text-white bg-orang-100 absolute top-4 z-10 rounded-lg px-2'> {category}</span>}
+                    <div className='absolute opacity-0 group-hover:opacity-100   z-10 transition ease-in-out delay-150 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-100%] group-hover:translate-y-[-50%]'>
+                        <BtnBasComponent basButtonsType='light' btnText='مشاهده' dyStyle='bg-wihte rounded-lg text-black px-8 my-4 transition ease-in-out delay-150 hover:bg-orang-100' size='sm'/>
+                        <BtnBasComponent basButtonsType='light' btnText='خریدن' dyStyle='bg-wihte rounded-lg text-black px-9 my-4  transition ease-in-out delay-150 hover:bg-orang-100' size='sm'/>
+                    </div>
+                </div>
+                <div className='flex flex-col px-4 py-2'>
+                    <span className='text-lg leading-7 py-2'>{orderName}</span>
+                    <span> قیمت : <span className={`${priceColor}`}>{orderPrice}  هزارتومان </span></span>
+                </div>
+            </div>
+        </Col>
     )
 }
